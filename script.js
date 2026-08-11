7d138a574ac0754dddb7b07b4cefa307ca816f10
Franka <fgriem@gmail.com>
2026-08-11 18:12:33 +0200

    POC: querying the API with parameters

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+async function queryVisualCrossing(searchTerm = 'Berlin', unitGroup = 'metric'){
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchTerm}?key=9FNW4QMFS9KMZ42V6738TXB6C&unitGroup=${unitGroup}`);
+        const asJson = await response.json();
+        console.log(asJson);
+    } catch(e){
+        console.error(e);
+    }
+};
+
+queryVisualCrossing();
